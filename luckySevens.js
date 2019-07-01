4a4c0e25eacd0ccacae9f3558324439f13d1f54c
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2019-06-30 20:15:35 -0500

    first commit

---
+++ luckySevens.js
@@ -0,0 +1,54 @@
+
+
+
+function play() {
+
+    var startBet = parseFloat(document.getElementById("starting-bet").value);
+    var money = startBet;
+    var diceSum = 0;
+    var maxMoney = 0;
+    var rolls = 0;
+    var maxRolls = 0;
+    if (money <= 0) {
+        alert("Please place a bid greater that $0")
+    }
+
+    while (money > 0) {
+        var diceOne = Math.floor(Math.random() * 6) + 1;
+        var diceTwo = Math.floor(Math.random() * 6) + 1;
+        diceSum = diceOne + diceTwo;
+        rolls++;
+        if (diceSum !=7) {
+            money--;
+        } else {
+            money += 4;
+            if(money > maxMoney){
+                maxMoney += (money - maxMoney)
+                maxRolls = rolls;
+            }
+        }
+
+    }
+
+   
+        document.getElementById("results").style.display = "block";
+        document.getElementById("startBet").value = "$" + startBet;
+        document.getElementById("totalRolls").value = rolls;
+        document.getElementById("maxMoney").value = "$" + maxMoney;
+        document.getElementById("maxRolls").value = maxRolls;
+    
+
+}
+
+function reset() {
+    document.getElementById("results").style.display = "none";
+    document.getElementById("startBet").value = 0;
+    document.getElementById("totalRolls").value = 0;
+    document.getElementById("maxMoney").value = 0;
+    document.getElementById("maxRolls").value = 0;
+}
+
+
+
+
+
